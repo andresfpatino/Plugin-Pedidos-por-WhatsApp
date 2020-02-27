1d475b7604c5417b71c20a6e143efe2792f60ce0
Andrés F. Patiño <andresfelipepatino5@gmail.com>
2020-02-27 14:35:09 -0500

    V1.0

---
+++ whatsapp-orders/readme.txt
@@ -0,0 +1,15 @@
+== Description ==
+
+The *Add Chat App Button* plugin enables adding a customizable click-to-chat WhatsApp button.
+The plugin lets you choose between a simple WhatsApp icon and a rectangle with a custom text label (see screenshots).
+
+== Usage == 
+
+The plugin settings page has two tabs: "General Settings" and "Button Design".
+
+*The General Settings tab* includes options such as limiting the button to only display under a certain screen resolution (in pixels), attaching a default message that will be populated in the user's phone when they click the button, and more.
+
+*The Button Design tab* includes controls for the button color, label text color, button type (side-bearing rectangle, WhatsApp icon, fixed-bottom button), and more. The Button Design tab also includes a smartphone mockup with a **live** preview screen, enabling you to see a real-time (estimated) rendering of how your button would look, on the fly, as you are changing its design.
+
+The plugin detects whether the site visitor is using a desktop or mobile device and serves a different link for each accordingly.
+
---
+++ whatsapp-orders/whatsapp-orders.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Plugin Name: Pedidos por WhatsApp 
+ * Plugin URI:  https://ecore.com.co
+ * Description: Cambia todos los productos a un botón "Pedidos por Whatsapp" en vez de agregar al carrito de compras.
+ * Version:     1.0
+ * Author:      Andrés Felipe Patiño
+ * Author URI:  https://ecore.com.co
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+
+defined('ABSPATH') or die("Bye bye");
+define('RUTA',plugin_dir_path(__FILE__));
+
+// Add menu page
+include(RUTA.'/includes/opciones.php');
+
+// Logica de la landing
+require_once(RUTA.'/front-button.php');
+
+
+// Registra cargar de scripts en el admin
+function plugin_scripts() {
+    wp_enqueue_style('pedido-whatsapp', plugin_dir_url( __FILE__ ).'includes/css/pedido-whatsapp.css', '', '1.0');
+}
+add_action('wp_enqueue_scripts', 'plugin_scripts');
+
+// Redirige a la página de opciones, luego de activar el plugin
+function activation_redirect( $plugin ) {
+    if( $plugin == plugin_basename( __FILE__ ) ) {
+        exit( wp_redirect( admin_url( 'admin.php?page=pedidos_por_whatsapp' ) ) );
+    }
+}
+add_action( 'activated_plugin', 'activation_redirect' );
